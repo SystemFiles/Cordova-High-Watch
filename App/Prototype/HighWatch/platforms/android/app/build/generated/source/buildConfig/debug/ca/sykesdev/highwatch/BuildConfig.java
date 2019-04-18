346263fbb9d347f42e352531d952e3277d102d3c
Ben Sykes <ben@sykesdev.ca>
2019-04-18 09:24:11 -0400

    Rebuild app for iOS

--- App/Prototype/HighWatch/platforms/android/app/build/generated/source/buildConfig/debug/ca/sykesdev/highwatch/BuildConfig.java
+++ App/Prototype/HighWatch/platforms/android/app/build/generated/source/buildConfig/debug/ca/sykesdev/highwatch/BuildConfig.java
@@ -11,5 +11,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 10000;
   public static final String VERSION_NAME = "1.0.0";
   // Fields from default config.
-  public static final long _BUILDINFO_TIMESTAMP = 1554944826342L;
+  public static final long _BUILDINFO_TIMESTAMP = 1555357472380L;
 }
